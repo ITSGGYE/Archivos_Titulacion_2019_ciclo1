4ba8e6be5cb868e9e75ac410e2ab73a50ca5e81d
Madhorns4 <52832651+Madhorns4@users.noreply.github.com>
2019-11-19 20:16:49 -0600

    Revert "Cobro_Pensiones_Escuela_2"
    This reverts commit 29a26867f15e8de5d79974c0263fc7ec36254a4a.

--- tesis_school/tesis_school/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from tesis_school.core.views import *
-from tesis_school.accounts.views import *
-
-urlpatterns = [
-	url(r'^$', index, name='index'),
-	url(r'^report/$', generate_xlsx_report, name='generate_xlsx_report'),
-]
