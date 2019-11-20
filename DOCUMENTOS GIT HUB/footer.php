68f6818c994b8045bf5e91f7fa5973c80cd0cd75
ITSGGYE <41635533+ITSGGYE@users.noreply.github.com>
2019-11-20 17:05:03 -0500

    Revert "Sistema Veterinario"
    This reverts commit c40ab2f8952c92a34ed8b5362c2e4cd94b5887af.

--- DOCUMENTOS GIT HUB/footer.php
+++
@@ -1,9 +0,0 @@
-
-<link rel="stylesheet" href="css/footer.css?nocache=" type="text/css" media="screen" />
-
-<body>
-    <footer>Desarrollado por Josué Montalván & Winston Montalvan
-        <a href="https://www.facebook.com/Veterinaria-Super-Doc-399967527292833/"><img border="0" src="images/FBPng.png" width="30em" height="30em"></a>
-        <a href="https://www.instagram.com/veterinaria_superdoc/"><img border="0" src="images/instagram_PNG10.png" width="30em" height="30em"></a>
-    </footer>
-
