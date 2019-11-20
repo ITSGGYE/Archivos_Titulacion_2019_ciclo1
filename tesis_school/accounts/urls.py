326b04787b1f056c83b45656bdcab7d20987887a
Madhorns4 <52832651+Madhorns4@users.noreply.github.com>
2019-11-19 20:12:59 -0600

    Revert "Cobro_Pensiones_Escuela_2"
    This reverts commit 4d9d079d1b44a4263786030a0e021c57550711fd.

--- tesis_school/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from tesis_school.accounts.views import *
-
-urlpatterns = [
-	#url(r'^$', dashboard, name="dashboard"),
-    url(r'^login/$', login, name='login'),
-    url(r'^logout/$', logout, name='logout'),    
-]
--- tesis_school/core/apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class InventaryConfig(AppConfig):
-    name = 'core'
